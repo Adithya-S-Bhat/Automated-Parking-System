2c46968b3b7a02d66dd32af19cdc1b9fa248abbe
Abhishek Aditya BS <abhishek.aditya10@gmail.com>
2022-03-16 00:00:37 +0530

    Add Service and Validator

--- automatedparkingsystem/src/main/java/com/parkingsystem/OutputPrinter.java
+++ automatedparkingsystem/src/main/java/com/parkingsystem/OutputPrinter.java
@@ -1,5 +1,41 @@
 package com.parkingsystem;
 
 public class OutputPrinter {
+
+    public void welcome() {
+        printWithNewLine("Welcome to the Parking Lot.");
+    }
+
+    public void end() {
+        printWithNewLine("Thank you for using our Parking Lot Service.");
+    }
+
+    public void notFound() {
+        printWithNewLine("Not found.");
+    }
+
+    public void statusHeader() {
+        printWithNewLine("Slot No.    Registration No    Colour");
+    }
+
+    public void parkingLotFull() {
+        printWithNewLine("Parking Lot is full.");
+
+    }
+
+    public void parkingLotEmpty() {
+        printWithNewLine("Parking Lot is empty.");
+    }
+    
+    public void invalidFile() {
+        printWithNewLine("Invalid file given.");
+    }
+
+    public void printWithNewLine(final String msg) {
+        System.out.println(msg);
+    
+    }
     
 }
+
+
